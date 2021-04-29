4c87d0197b4878271a198609aa72449b2653bdba
Lord-Y <Lord-Y@users.noreply.github.com>
2021-04-29 17:36:26 +0200

    [sql] - Fix sql typo

--- sql/postgres/1_create_table_projects.up.sql
+++ sql/postgres/1_create_table_projects.up.sql
@@ -1,5 +1,5 @@
-CREATE TABLE teams (
-  team_id SERIAL PRIMARY KEY,
-  team_name varchar(100) NOT NULL,
+CREATE TABLE projects (
+  project_id SERIAL PRIMARY KEY,
+  project_name varchar(100) NOT NULL,
   date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
