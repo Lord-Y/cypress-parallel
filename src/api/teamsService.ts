af067abc7eccf50b6fbe573c7c186ca51c3fdbca
Lord-Y <Lord-Y@users.noreply.github.com>
2021-05-23 10:37:49 +0200

    [api] - Update teamService requirements

--- src/api/teamsService.ts
+++ src/api/teamsService.ts
@@ -7,6 +7,12 @@ export interface Teams {
   date: Date
 }
 
+export interface Team {
+  team_id: number
+  team_name: string
+  date: Date
+}
+
 const axiosClient: AxiosInstance = axios.create({
   headers: {
     'Content-type': 'application/json',
@@ -14,18 +20,24 @@ const axiosClient: AxiosInstance = axios.create({
 })
 
 class TeamsService {
-  async get(page = 1): Promise<any> {
-    return await axiosClient.get<any>(
-      `/api/v1/cypress-parallel-api/teams?page=${page}`,
-    )
-  }
-
   async create(data: any): Promise<any> {
     return await axiosClient.post('/api/v1/cypress-parallel-api/teams', data)
   }
 
-  async update(id: number): Promise<any> {
-    return await axiosClient.put(`/api/v1/cypress-parallel-api/teams/${id}`)
+  async get(id: number): Promise<any> {
+    return await axiosClient.get<any>(
+      `/api/v1/cypress-parallel-api/teams/${id}`,
+    )
+  }
+
+  async list(page = 1): Promise<any> {
+    return await axiosClient.get<any>(
+      `/api/v1/cypress-parallel-api/teams/list?page=${page}`,
+    )
+  }
+
+  async update(data: any): Promise<any> {
+    return await axiosClient.put('/api/v1/cypress-parallel-api/teams', data)
   }
 
   async delete(id: number): Promise<any> {
