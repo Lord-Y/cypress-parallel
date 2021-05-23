ad578f8f6560d9b6a82000cb9fd402506b44e106
Lord-Y <Lord-Y@users.noreply.github.com>
2021-05-23 10:40:10 +0200

    [components|buttons] - Add generic buttons

---
+++ src/components/buttons/SubmitButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="mt-4">
+    <button type="submit" :class="classes">
+      {{ text }}
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    classes: {
+      type: String,
+      default: 'w-full p-3 bg-gray-900 text-white hover:text-green-500',
+    },
+  },
+})
+</script>
