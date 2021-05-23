743630f83a3b4caa585478d5255b23635410f03c
Lord-Y <Lord-Y@users.noreply.github.com>
2021-05-23 10:37:12 +0200

    [vee-validate] - Add vee-validate requirements

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,28 @@ import router from './router'
 import { createI18n } from 'vue-i18n'
 import messages from '@intlify/vite-plugin-vue-i18n/messages'
 import './tailwind.css'
+import { defineRule, configure } from 'vee-validate'
+import { localize } from '@vee-validate/i18n'
+import AllRules from '@vee-validate/rules'
+import en from '@vee-validate/i18n/dist/locale/en.json'
+import fr from '@vee-validate/i18n/dist/locale/fr.json'
 const i18n = createI18n({
   legacy: false,
   locale: 'en-US',
   fallbackLocale: 'en-US',
   globalInjection: true,
   messages,
+})
+
+configure({
+  generateMessage: localize({
+    en,
+    fr,
+  }),
+})
+
+Object.keys(AllRules).forEach((rule) => {
+  defineRule(rule, AllRules[rule])
 })
 
 const app = createApp(App)
