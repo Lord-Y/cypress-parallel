cff3ba9570716bc4ad8beb6d3e5391fa038b5481
Lord-Y <Lord-Y@users.noreply.github.com>
2021-05-26 17:34:11 +0200

    [components|commons] - Add new common components

---
+++ src/components/commons/CreateUpdateKey.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="mt-4">
+    <label :for="id" class="block w-full pb-1">{{
+      translation
+    }}</label>
+    <Field
+      as=""
+      :name="name"
+      v-model="local"
+      :label="translation.toLowerCase()"
+      rules="required"
+      v-slot="{ field, meta, errorMessage }"
+      type="text"
+    >
+      <input
+        v-bind="field"
+        type="text"
+        :id="id"
+        :placeholder="translation.toLowerCase()"
+        autocomplete="off"
+        class="
+          block
+          w-full
+          border-gray-300
+          focus:outline-none
+          focus:border-green-500
+          focus:ring-green-500
+        "
+        :class="getValidationClass(meta)"
+      />
+      <span v-if="errorMessage" class="text-red-500">{{ errorMessage }}</span>
+    </Field>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+import { Field } from 'vee-validate'
+
+export default defineComponent({
+  props: {
+    id: {
+      type: String,
+      default: 'key',
+    },
+    name: {
+      type: String,
+      default: 'key',
+    },
+    kkey: {
+      type: String,
+      default: '',
+    },
+    translation: {
+      type: String,
+      required: true,
+    },
+  },
+  components: {
+    Field,
+  },
+  emits: ['update:updateKkey'],
+  setup(props, { emit }) {
+    const local = computed({
+      get: () => {
+        emit('update:updateKkey', props.kkey)
+        return props.kkey
+      },
+      set: (value: string) => emit('update:updateKkey', value),
+    })
+    function getValidationClass(meta: any): string {
+      if (meta.valid && meta.validated && meta.dirty) {
+        return 'outline-none border-green-500'
+      }
+      if (!meta.valid && !meta.validated && meta.dirty) {
+        return 'outline-none border-red-500'
+      }
+      return ''
+    }
+
+    return {
+      local,
+      getValidationClass,
+    }
+  },
+})
+</script>
