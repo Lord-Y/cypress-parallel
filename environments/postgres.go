2c3de0a62b421be8ce35eef64211ba13f5468536
Lord-Y <Lord-Y@users.noreply.github.com>
2021-05-26 18:40:34 +0200

    [environments] - Read must return an object no array of objects

--- environments/postgres.go
+++ environments/postgres.go
@@ -128,7 +128,7 @@ func (p *listEnvironments) list() (z []map[string]interface{}, err error) {
 }
 
 // read will return all environments with range limit settings
-func (p *getEnvironments) read() (z []map[string]interface{}, err error) {
+func (p *getEnvironments) read() (z map[string]string, err error) {
 	db, err := sql.Open(
 		"postgres",
 		commons.BuildDSN(),
@@ -163,23 +163,21 @@ func (p *getEnvironments) read() (z []map[string]interface{}, err error) {
 		scanArgs[i] = &values[i]
 	}
 
-	m := make([]map[string]interface{}, 0)
+	m := make(map[string]string)
 	for rows.Next() {
 		err = rows.Scan(scanArgs...)
 		if err != nil {
-			return z, err
+			return
 		}
 		var value string
-		sub := make(map[string]interface{})
 		for i, col := range values {
 			if col == nil {
 				value = ""
 			} else {
 				value = php2go.Stripslashes(string(col))
 			}
-			sub[columns[i]] = value
+			m[columns[i]] = value
 		}
-		m = append(m, sub)
 	}
 	if err = rows.Err(); err != nil {
 		return z, err
