592b3d8c08e56163a4cf99b667d4894da30d6be6
Lord-Y <Lord-Y@users.noreply.github.com>
2021-05-26 11:42:33 +0200

    [tools] - Add tools to get status of executions

---
+++ src/tools/status.ts
@@ -0,0 +1,64 @@
+import { Execution } from '../api/executionsService'
+
+const executionStatus: Array<string> = [
+  'NOT_STARTED',
+  'SCHEDULED',
+  'RUNNING',
+  'FAILED',
+  'CANCELLED',
+]
+
+class Statuses {
+  tests(execution: Execution): string {
+    if (
+      Object.keys(execution).length > 0 &&
+      executionStatus.includes(execution.execution_status)
+    ) {
+      return execution.execution_status
+    }
+
+    if (Object.keys(execution.result).length > 0) {
+      const result: any = JSON.parse(execution.result)
+      let passes = 0
+      const total: number = Object.keys(
+        result.results[0].suites[0].tests,
+      ).length
+      for (let i = 0; i < total; i++) {
+        if (result.results[0].suites[0].tests[i].pass) {
+          passes += 1
+        }
+      }
+      return passes + ' passes / ' + total
+    }
+    return ''
+  }
+
+  global(execution: Execution): string {
+    if (
+      Object.keys(execution).length > 0 &&
+      execution.execution_status === 'FAILED'
+    ) {
+      return execution.execution_status
+    }
+
+    if (Object.keys(execution.result).length > 0) {
+      const result: any = JSON.parse(execution.result)
+      let passes = 0
+      const total: number = Object.keys(
+        result.results[0].suites[0].tests,
+      ).length
+      for (let i = 0; i < total; i++) {
+        if (result.results[0].suites[0].tests[i].pass) {
+          passes += 1
+        }
+      }
+      if (passes === total) {
+        return 'PASSED'
+      }
+      return 'FAILED'
+    }
+    return ''
+  }
+}
+
+export default new Statuses()
