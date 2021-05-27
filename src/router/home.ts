d01d679e74afdd9fc462f1a7e734dd7cd71f4949
Lord-Y <Lord-Y@users.noreply.github.com>
2021-05-27 10:54:52 +0200

    [routers] - Redirect home page to execution list

--- src/router/home.ts
+++ src/router/home.ts
@@ -1,15 +1,9 @@
 import { RouteRecordRaw } from 'vue-router'
-import Home from '@views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
-    meta: {
-      root: 'home',
-      activeLink: '/',
-    },
+    redirect: '/executions/list',
   },
 ]
 
