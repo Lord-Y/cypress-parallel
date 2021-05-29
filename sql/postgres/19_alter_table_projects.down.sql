70766371d8ce9e9b97e4688d53cb6ac600cdda8d
Lord-Y <Lord-Y@users.noreply.github.com>
2021-05-29 09:44:13 +0200

    [sql] - Add browser/config file fields

---
+++ sql/postgres/19_alter_table_projects.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE projects DROP COLUMN IF EXISTS browser;
+ALTER TABLE projects DROP COLUMN IF EXISTS config_file;
---
+++ sql/postgres/19_alter_table_projects.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE projects ADD browser VARCHAR(100) DEFAULT 'chrome';
+ALTER TABLE projects ADD config_file VARCHAR(100) DEFAULT 'cypress.json';
