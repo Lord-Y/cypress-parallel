b1a6f8f145b104a04ac5ecefb369d845ad712cc2
Lord-Y <Lord-Y@users.noreply.github.com>
2021-06-04 18:00:55 +0200

    [circleci] - Update golang script

--- .circleci/golang.sh
+++ .circleci/golang.sh
@@ -2,46 +2,29 @@
 
 VERSION="$1"
 
-install()
+install_go()
 {
   mkdir -p ~/go/bin
-  sudo rm -rf /usr/local/go ~/.cache/go-build/* ~/go/pkg/mod/* ~/go/src/* /tmp/go${VERSION}.linux-amd64.tar.gz
+  test -f /usr/bin/sudo && sudo rm -rf /usr/local/go ~/.cache/go-build/* ~/go/* /tmp/go${VERSION}.linux-amd64.tar.gz || rm -rf /usr/local/go ~/.cache/go-build/* ~/go/* /tmp/go${VERSION}.linux-amd64.tar.gz
   wget https://golang.org/dl/go${VERSION}.linux-amd64.tar.gz -P /tmp
-  sudo tar -C /usr/local -xzf /tmp/go${VERSION}.linux-amd64.tar.gz
+
+  test -f /usr/bin/sudo && sudo tar -C /usr/local -xzf /tmp/go${VERSION}.linux-amd64.tar.gz || tar -C /usr/local -xzf /tmp/go${VERSION}.linux-amd64.tar.gz
   rm -rf /tmp/go${VERSION}.linux-amd64.tar.gz
-  grep /usr/local/go/bin ~/.bashrc &> /dev/null
-  if [ $? eq 1 ]
+  grep /usr/local/go/bin ~/.bashrc
+  if [ $? -eq 1 ]
   then
     echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
     echo 'export PATH=$PATH:$HOME/go/bin' >> ~/.bashrc
     echo 'export GO111MODULE=on' >> ~/.bashrc
-    source ~/.bashrc
+    . ~/.bashrc
   fi
-}
-
-check()
-{
   which go
-}
-
-check_installed_version()
-{
-  go version |grep $VERSION &>/dev/null
 }
 
 if [ -n "$VERSION" ]
 then
-  which go &> /dev/null
-  if [ $? eq 1 ]
-  then
-    install
-  fi
-  check_installed_version
-  if [ $? eq 1 ]
-  then
-    install
-  fi
-  go version
+  which go || install_go
+  go version |grep "$VERSION" || install_go
 else
   echo "golang version is missing"
   exit 1
